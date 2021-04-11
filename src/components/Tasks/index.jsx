de6d6d1aacb5eab9f0962f9a6a675cf5eff38239
alexeyinn <69390951+alexeyinn@users.noreply.github.com>
2021-04-11 18:58:12 +0300

    refactoring: "Задачи" в отдельнеый компонент

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -4,6 +4,7 @@ import "./Tasks.scss";
 import editSVG from "../../assets/img/edit.svg";
 
 import AddTaskForm from "./AddTaskForm";
+import Task from "./Task";
 
 export default function Tasks({ list, onEditTitle, onAddTask, withoutEmpty }) {
   const editTitle = () => {
@@ -31,29 +32,7 @@ export default function Tasks({ list, onEditTitle, onAddTask, withoutEmpty }) {
       <div className="tasks__items">
         {!withoutEmpty && !list.tasks.length && <h2>Задачи отсутсвуют</h2>}
         {list.tasks.map((task) => (
-          <div key={task.id} className="tasks__items-row">
-            <div className="checkbox">
-              <input id={`task-${task.id}`} type="checkbox" />
-              <label htmlFor={`task-${task.id}`}>
-                <svg
-                  width="11"
-                  height="8"
-                  viewBox="0 0 11 8"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001"
-                    stroke="#000"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </label>
-            </div>
-            <input readOnly value={task.text} />
-          </div>
+          <Task key={task.id} {...task} />
         ))}
         <AddTaskForm list={list} onAddTask={onAddTask} />
       </div>
